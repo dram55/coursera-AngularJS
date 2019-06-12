8af52c9d2335460e6819d987aab287811149c06f
dram55 <isbister1500@hotmail.com>
2019-06-11 23:00:59 -0400

    Initial Commit

---
+++ module1-solution/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var app = angular.module("lunchCheck", []);
+
+app.controller("LunchCheckController", LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+   var errorMessage = "Too Much!";
+   var successMessage = "Enjoy";
+
+   $scope.items = "";
+
+   $scope.checkIfTooMuchFood = function () {
+      var itemArray = $scope.items.split(",").filter(element => element.trim().length > 0);
+
+      if (itemArray.length > 3) {
+         $scope.message = errorMessage;
+      } else if (itemArray.length > 0 && itemArray.length <= 3) {
+         $scope.message = successMessage;
+      } else {
+         $scope.message = "Please enter data first";
+      }
+   }
+
+   $scope.isSuccessMessage = function(message) {
+      if(message === successMessage) return true;
+      else return false;
+   }
+}
